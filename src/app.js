0a80dd2faf26b75dbb3420f022e22509bcc0bdaa
Nitin Reddy Kamsani <kamsaninitinreddy@gmail.com>
2025-07-07 19:49:12 +0530

    Error Handling

--- src/app.js
+++ src/app.js
@@ -7,7 +7,16 @@ const {isAdmin} = require("./middlewares/auth")
 app.use("/admin",isAdmin)
 
 app.get("/admin/getUsers",(req,res)=>{
+
+    try {
+
+    throw new error("hfief")
     res.send("Users retreived");
+
+    }
+    catch(err){
+        res.status(500).send("Unexpected error occured");
+    }
 })
 
 app.listen(7777,()=>{
