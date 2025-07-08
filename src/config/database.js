8a8db00e603063cfa24e50657a89c300a010b4ad
Nitin Reddy Kamsani <kamsaninitinreddy@gmail.com>
2025-07-08 13:34:44 +0530

    creating user schema

--- src/config/database.js
+++ src/config/database.js
@@ -3,11 +3,7 @@ const mongoose = require('mongoose');
 
 async function connectDB() {
 
-<<<<<<< HEAD
     await mongoose.connect("uri");
-=======
-    await mongoose.connect(uri);
->>>>>>> acf8be7 ( creating user schema and apis)
 }
 
 module.exports = connectDB
