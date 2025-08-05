81a39b39986a7c18b244ab8fe1d5daaf6b8fb74b
Nitin Reddy Kamsani <kamsaninitinreddy@gmail.com>
2025-08-05 15:52:28 +0530

    connection request api

--- src/routes/requestRoute.js
+++ src/routes/requestRoute.js
@@ -2,6 +2,7 @@ const express = require('express');
 const Requestrouter = express.Router();
 const {userAuth} = require('../middlewares/auth');
 const ConnectionRequest = require('../models/connectionRequest');
+const User = require("../models/User")
 
 Requestrouter.post("/sendconnectionrequest", userAuth, (req,res)=>{
 
@@ -15,30 +16,29 @@ Requestrouter.post("/request/send/:status/:toid",userAuth,async(req,res)=>{
         const toId = req.params.toid;
         const status = req.params.status;
 
+        const toUser = await User.findById(toId);
+        if(!toUser){
+            res.status(404).json({message:"User not found"});
+        }
+
         const allowedUpdates = ["ignored","interested"];
         if(!allowedUpdates.includes(status)){
             return res.status(400).json({message:"Invalid status"});
         }
-
         const idExists = await ConnectionRequest.findOne({
             fromConnectionId : fromId,
             toConnectionId : toId
         })
-
         const reverseConection = await ConnectionRequest.findOne({
             fromConnectionId : toId,
             toConnectionId : fromId
         })
-
-
         if(idExists) {
             return res.status(400).json({message:"Request already sent"})
         }
         if(fromId === toId){
             return res.status(400).json({message:"You cannot send request to yourself"})
         }
-
-        
         if(reverseConection){
             return res.status(400).json({message:"You cant send invite,please accept if interested"});
         }
