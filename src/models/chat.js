e220c855b645a6848fe1ab0e34a8d4c9400f2841
Nitin Reddy Kamsani <kamsaninitinreddy@gmail.com>
2025-09-01 12:34:38 +0530

    Implementing web sockets

---
+++ src/models/chat.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const messageSchema = mongoose.Schema({
+
+    senderId :  {
+        type : mongoose.Schema.Types.ObjectId,
+        ref : "User",
+        required : true
+    },
+    text : {
+        type : String,
+        required : true
+    }
+})
+
+const chatSchema = mongoose.Schema({
+
+
+    participants : [{type : mongoose.Schema.Types.ObjectId, ref : "User" , required : true}]
+
+})
+
+const Chat = mongoose.model("chat", chatSchema);
+
+module.exports = {Chat}
